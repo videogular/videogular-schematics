6d5ce8ea1669ef5383c819cfe30fae0c0380aec8
Jorge Cano <jorgeucano@gmail.com>
2018-02-22 00:23:30 -0300

    feat(template): add if by classify

--- src/vg/files/vg-component/vg.component.ts
+++ src/vg/files/vg-component/vg.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 
+<% if (VgService) { %>
+    import { VgService } from './vg.service';
+<% } %>
+
 @Component({
     selector: 'vg-component',
     templateUrl: './app.component.html',
-    styleUrls: [ './app.component.css' ]
+    styleUrls: [ './app.component.css' ],
+    providers: [VgService]
 })
+
 export class VgComponent {
     sources: Array<Object>;
     constructor() {
+        <% if (VgService) { %>
+            console.log('if work fine');
+        <% } %>
+
         this.sources = [
             {
                 src: "http://static.videogular.com/assets/videos/videogular.mp4",
